aa010be11146ca625545a506c7d47058e7da5042
backendprojekt <nala.bibliander@gmail.com>
2024-05-18 14:10:23 +0200

    get data from database

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
 
+hostname = 'localhost'
+user = 'macromedia'
+password = 'macromedia'
+mydatabase = 'JuggerTube'
 
-@app.route('/')
-def hello():
-    return 'Hello World'
+
+def execute_query(query):
+    conn = mysql.connector.connect(
+        host=hostname,
+        user=user,
+        password=password,
+        database=mydatabase
+    )
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        conn.commit()
+        return result, columns
+    except Error as e:
+        conn.rollback()
+        return {'error': str(e)}, 400
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+
+
+@app.route('/query', methods=['GET'])
+def query():
+    sql_query = request.args.get('query')
+    if not sql_query:
+        return jsonify({'error': 'No SQL query provided'}), 400
+
+    results, columns = execute_query(sql_query)
+    if isinstance(results, tuple) and 'error' in results[0]:
+        return jsonify({'error': results[0]['error']}), results[1]
+    return jsonify({'results': results, 'columns': columns})
 
 
 if __name__ == '__main__':

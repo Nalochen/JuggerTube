31b15160fea0dd7e6be79469211848bbbaeb16b1
backendprojekt <nala.bibliander@gmail.com>
2024-06-04 21:19:57 +0200

    pytest teams wip

--- tests/test_teams.py
+++ tests/test_teams.py
@@ -1,45 +1,50 @@
 import pytest
 
 
-class TestTeamsRoute:
-    def test_get_all_teams(self):
-        response = client.get('/teams')
-        assert response.status_code == 200
+def test_get_all_teams(client):
+    response = client.get('/teams')
+    assert response.status_code == 200
 
-    def test_get_add_team(self):
-        response = client.get('/teams/add')
-        assert response.status_code == 200
 
-    def test_post_add_team_no_data(self):
-        response = client.post('/teams/add',
-                               )
-        assert response.status_code == 200
+def test_get_add_team(client):
+    response = client.get('/teams/add')
+    assert response.status_code == 200
 
-    def test_post_add_team_works(self):
-        response = client.post('/teams/add',
-                               )
-        assert response.status_code == 200
 
-    def test_get_edit_team_no_data(self):
-        response = client.post('/teams/edit',
-                               )
-        assert response.status_code == 200
+@pytest.mark.parametrize('test_input, expected',
+                         [
+                             ({'name': 'Pink Pain', 'city': 'Darmstadt', 'country': 'Germany'}, 200),
+                             ({'name': '', 'city': '', 'country': ''}, 404),
+                             ({'name': '12', 'city': '13', 'country': 'Germany'}, 500),
+                         ]
+                         )
+def test_post_add_team(client, test_input, expected):
+    assert client.post('/teams/add', data=test_input).status_code == expected
 
-    def test_post_edit_team_team_doesnt_exist(self):
-        response = client.post('/teams/edit',
-                               )
-        assert response.status_code == 200
 
-    def test_post_edit_team_no_current_user(self):
-        response = client.post('/teams/edit',
-                               )
-        assert response.status_code == 200
+def test_get_edit_team(client):
+    response = client.get('/teams/edit')
+    assert response.status_code == 200
 
-    def test_post_edit_team_works(self):
-        response = client.post('/teams/edit',
-                               )
-        assert response.status_code == 200
 
-    def test_delete_team(self):
-        response = client.get('/teams/delete')
-        assert response.status_code == 200
+@pytest.mark.parametrize('test_input, expected',
+                         [
+                             ({'id': 1, 'name': 'Pink Pain', 'city': 'Darmstadt', 'country': 'Germany',
+                               'current_user': 1}, 200),
+                             ({'id': 5, 'name': 'Pink Pain', 'city': 'Darmstadt', 'country': 'Germany',
+                               'current_user': 1}, 404),
+                             ({'id': 1, 'name': '', 'city': '', 'country': '',
+                               'current_user': 1}, 404),
+                             ({'id': 1, 'name': 'Pink Pain', 'city': 'Darmstadt', 'country': 'Germany',
+                               'current_user': 5}, 200),
+                         ]
+                         )
+def test_post_edit_team(client, test_input, expected):
+    assert client.post('/teams/edit', data=test_input).status_code == expected
+
+
+def test_delete_team(client):
+    # video existiert
+    # video existiert nicht
+    response = client.get('/teams/delete')
+    assert response.status_code == 200

56f2fefcc96be628e9ef0fdc62f44e2178b1dc18
Nalo Unger <nalo.unger@check24.de>
2024-06-21 00:14:49 +0200

    swagger ui docu

--- juggertube/endpoints/general/general_blueprint.py
+++ juggertube/endpoints/general/general_blueprint.py
@@ -5,4 +5,9 @@ general_blueprint = Blueprint('general', __name__, template_folder='templates')
 
 @general_blueprint.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('swaggerui.html')
+
+
+@general_blueprint.route('/api/docs', methods=['GET'])
+def get_docs():
+    return render_template('swaggerui.html')

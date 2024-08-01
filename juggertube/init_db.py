27cce518e1733ae6fc1f28f7042f313d37146baf
Nalo Unger <nalo.unger@check24.de>
2024-08-01 19:53:16 +0200

    [main] import data

--- juggertube/init_db.py
+++ juggertube/init_db.py
@@ -1,5 +1,3 @@
-from flask import flash
-
 from juggertube.data import (users, channels, teams, tournaments, init_videos)
 from juggertube.models import db
 
@@ -8,10 +6,10 @@ def init_db(app):
     with app.app_context():
         db.create_all()
 
-        #teams.init_teams(app)
-        #tournaments.init_tournaments(app)
-        #users.init_users(app)
-        #channels.init_channels(app)
-        #init_videos.init_videos(app)
+        teams.init_teams(app)
+        tournaments.init_tournaments(app)
+        users.init_users(app)
+        channels.init_channels(app)
+        init_videos.init_videos(app)
 
         db.session.commit()

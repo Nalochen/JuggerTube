d8f19ec09fa040d1c220d6c0a0dc85d2ef6bbc61
Nalo Unger <nalo.unger@check24.de>
2025-04-06 13:06:51 +0200

    [10-api-für-übersichtsseite] use ne database relation data for api

--- backend/DataDomain/Database/Repository/VideoRepository.py
+++ backend/DataDomain/Database/Repository/VideoRepository.py
@@ -3,6 +3,12 @@ from typing import List
 from DataDomain.Database import db
 from DataDomain.Database.Model.Videos import Videos
 
+from DataDomain.Database.Model.Channels import Channels
+from DataDomain.Database.Model.Tournaments import Tournaments
+
+from DataDomain.Database.Model.Teams import Teams
+
+
 class VideoRepository:
     """Repository for video related queries"""
 
@@ -10,7 +16,7 @@ class VideoRepository:
     def getVideoOverview() -> List[dict]:
         """Get Video Overview"""
 
-        videos = db.session.query(
+        videos = (db.session.query(
             Videos.id,
             Videos.name,
             Videos.category,
@@ -21,14 +27,29 @@ class VideoRepository:
             Videos.game_system,
             Videos.weapon_type,
             Videos.topic,
-            Videos.guests
+            Videos.guests,
+            Channels.name.label('channel_name'),
+            Tournaments.name.label('tournament_name'),
+            Teams.name.label('team_name')
+        ).join(
+            Channels,
+            Videos.channel_id == Channels.id
+        ).join(
+            Tournaments,
+            Videos.tournament_id == Tournaments.id
+        ).join(
+            Teams,
+            Videos.team_one_id == Teams.id
+        ).join (
+            Teams,
+            Videos.team_two_id == Teams.id
         ).filter(
             Videos.is_deleted is False
         ).group_by(
             Videos.id
         ).order_by(
             Videos.upload_date
-        ).all()
+        ).all())
 
         return [{
             'id': video.id,
@@ -41,5 +62,8 @@ class VideoRepository:
             'gameSystem': video.game_system,
             'weaponType': video.weapon_type,
             'topic': video.topic,
-            'guests': video.guests
+            'guests': video.guests,
+            'channelName': video.channel_name,
+            'tournamentName': video.tournament_name,
+            'teamName': video.team_name,
         } for video in videos]

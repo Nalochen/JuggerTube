2e8a56190e0e2815d9d6e0f8e1fd7bfd3995f17f
Nalo Unger <nalo.unger@check24.de>
2025-04-12 12:29:45 +0200

    [22-api-für-videoinputs] add get all teams function

--- backend/DataDomain/Database/Repository/TeamRepository.py
+++ backend/DataDomain/Database/Repository/TeamRepository.py
@@ -1,10 +1,39 @@
 from DataDomain.Database.db import db
 from DataDomain.Database.Model import Teams
 from Infrastructure.Logger.Logger import logger
+from typing import List
 
 
 class TeamRepository:
     """Repository for team related queries"""
+
+    @staticmethod
+    def getAllTeams() -> List[dict]:
+        """Get all teams from database"""
+        teams = (db.session.query(
+            Teams.id,
+            Teams.name,
+            Teams.country,
+            Teams.city,
+            Teams.comment
+        ).filter(
+            Teams.is_deleted != True
+        ).order_by(
+            Teams.name
+        ).all())
+
+        result = []
+        for team in teams:
+            team_dict = {
+                'id': team.id,
+                'name': team.name,
+                'country': team.country,
+                'city': team.city,
+                'comment': team.comment
+            }
+            result.append(team_dict)
+
+        return result
 
     @staticmethod
     def create(team: Teams) -> int:

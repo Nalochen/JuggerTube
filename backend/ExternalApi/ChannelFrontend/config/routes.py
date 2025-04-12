ec355acb775efe0f8e7c47fb4c632f4a0b5dd5f9
Nalo Unger <nalo.unger@check24.de>
2025-04-12 15:51:41 +0200

    [21-api-um-videodetails-ins-fe-zu-bekommen] add cache to all overview routes

--- backend/ExternalApi/ChannelFrontend/config/routes.py
+++ backend/ExternalApi/ChannelFrontend/config/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from config import cache
 
 from DataDomain.Model import Response
 from ExternalApi.ChannelFrontend.Handler.GetChannelOverviewHandler import GetChannelOverviewHandler
@@ -9,5 +10,6 @@ channel_frontend = Blueprint('channel-frontend', __name__)
 
 @channel_frontend.route('/get-channel-overview',
                         methods=['GET'], endpoint='get-channel-overview')
+@cache.cached(key_prefix='channel-overview')
 def getChannelOverview() -> Response:
     return GetChannelOverviewHandler.handle()
--- backend/ExternalApi/TeamFrontend/config/routes.py
+++ backend/ExternalApi/TeamFrontend/config/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from config import cache
 
 from DataDomain.Model import Response
 from ExternalApi.TeamFrontend.Handler.GetTeamOverviewHandler import GetTeamOverviewHandler
@@ -8,5 +9,6 @@ team_frontend = Blueprint('team-frontend', __name__)
 
 @team_frontend.route('/get-team-overview',
                       methods=['GET'], endpoint='get-team-overview')
+@cache.cached(key_prefix='team-overview')
 def getTeamOverview() -> Response:
     return GetTeamOverviewHandler.handle()

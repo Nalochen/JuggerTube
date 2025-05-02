1370c664454221094468799e1af864ac026bc777
Nalochen <unger.nalo@gmx.de>
2025-05-02 14:18:11 +0200

    [33-frontend-überarbeiten] pagination works

--- frontend/apps/desktop/src/app/pages/page-video-overview/page-video-overview.component.ts
+++ frontend/apps/desktop/src/app/pages/page-video-overview/page-video-overview.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, Signal } from '@angular/core';
+import { Component, Signal, computed, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { SearchVideoTileComponent } from './components/search-video-tile/search-video-tile.component';
 import { VideoTileComponent } from './components/video-tile/video-tile.component';
 import { VideosDataService } from '@frontend/video';
 import { VideoApiResponseModel } from '@frontend/video-data';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @Component({
   imports: [
@@ -13,6 +14,7 @@ import { VideoApiResponseModel } from '@frontend/video-data';
     VideoTileComponent,
     SearchVideoTileComponent,
     RouterLink,
+    MatPaginatorModule
   ],
   standalone: true,
   templateUrl: './page-video-overview.component.html',
@@ -20,9 +22,32 @@ import { VideoApiResponseModel } from '@frontend/video-data';
 })
 export class PageVideoOverviewComponent {
   public readonly videos: Signal<VideoApiResponseModel[]>;
+  public readonly paginatedVideos: Signal<VideoApiResponseModel[]>;
+  public readonly pageSizeOptions = [5, 10, 25, 50];
+  public readonly showFirstLastButtons = true;
+  private readonly _pageSize = signal(10);
+  private readonly _pageIndex = signal(0);
+
+  public get pageSize(): number {
+    return this._pageSize();
+  }
+
+  public get pageIndex(): number {
+    return this._pageIndex();
+  }
 
   constructor(private readonly videosDataService: VideosDataService) {
     this.videosDataService.loadVideos();
     this.videos = this.videosDataService.videos;
+    this.paginatedVideos = computed(() => {
+      const start = this._pageIndex() * this._pageSize();
+      const end = (this._pageIndex() + 1) * this._pageSize();
+      return this.videos().slice(start, end);
+    });
+  }
+
+  public handlePageEvent(event: any): void {
+    this._pageSize.set(event.pageSize);
+    this._pageIndex.set(event.pageIndex);
   }
 }
